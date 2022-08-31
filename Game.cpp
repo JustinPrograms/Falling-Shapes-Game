c55a64eaa7a46fa36ad4e3dc0f797ac7d01ae4a3
BuildTools <unconfigured@null.spigotmc.org>
2022-08-31 03:25:41 -0400

    Random enemy sizes

--- Game.cpp
+++ Game.cpp
@@ -87,35 +87,43 @@ void Game::spawnEnemy() {
 
     
     // Randomize enemy type
-    int type = rand() % 5;
+    int type = rand() % 6;
+    this->enemy.setSize(sf::Vector2f(100.f, 100.f));
+
+    float sizeFloat = static_cast<float>(rand() % 10) / 9.f;
+    sf::Vector2f size = sf::Vector2f(sizeFloat, sizeFloat);
+
+
+    this->enemy.setSize(sf::Vector2f(100.f, 100.f));
+    this->enemy.setScale(sf::Vector2f(1.f, 1.f));
 
     switch (type) {
     case 0:
-        this->enemy.setSize(sf::Vector2f(10.f, 10.f));
+        this->enemy.setScale(sf::Vector2f(0.10f, 0.10f));
         this->enemy.setFillColor(sf::Color::Magenta);
         break;
     case 1:
-        this->enemy.setSize(sf::Vector2f(30.f, 30.f));
+        this->enemy.setScale(size);
         this->enemy.setFillColor(sf::Color::Blue);
         break;
     case 2:
-        this->enemy.setSize(sf::Vector2f(50.f, 50.f));
+        this->enemy.setScale(size);
         this->enemy.setFillColor(sf::Color::Cyan);
         break;
     case 3:
-        this->enemy.setSize(sf::Vector2f(70.f, 70.f));
+        this->enemy.setScale(size);
         this->enemy.setFillColor(sf::Color(255, 165, 0, 255));
         break;
     case 4:
-        this->enemy.setSize(sf::Vector2f(100.f, 100.f));
+        this->enemy.setScale(size);
         this->enemy.setFillColor(sf::Color::Green);
         break;
     case 5:
-        this->enemy.setSize(sf::Vector2f(30.f, 30.f));
+        this->enemy.setScale(size);
         this->enemy.setFillColor(sf::Color::Red);
         break;
     default:
-        this->enemy.setSize(sf::Vector2f(100.f, 100.f));
+        this->enemy.setScale(size);
         this->enemy.setFillColor(sf::Color::Yellow);
         break;
 
@@ -204,7 +212,6 @@ void Game::updateEnemies() {
                     } 
                     else if (this->enemies[i].getFillColor() == sf::Color::Blue) {
                         this->points += 5;
-                        this->health += 1;
                     }
                     else if (this->enemies[i].getFillColor() == sf::Color::Cyan) {
                         this->points += 3;
